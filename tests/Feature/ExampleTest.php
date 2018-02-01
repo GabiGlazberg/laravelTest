5c50f3e4061fadd2bc0bb03819741104753e2284
gabiGlazberg <gglazberg@gmail.com>
2018-02-01 17:23:12 +0200

    take it home and keep working on it

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -15,18 +14,5 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-
-        $first = factory(Post::class)->create();
-
-        $secund = factory(Post::class)->create([
-           'create_at' => \Carbon\Carbon::now()->subMonth()
-        ]);
-
-        $posts = Post::archives();
-
-        $this->assertCount(2, $posts);
     }
 }
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,11 +2,16 @@
 
 namespace Tests\Unit;
 
+use App\Post;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+
+    use DatabaseTransactions;
     /**
      * A basic test example.
      *
@@ -15,5 +20,19 @@ class ExampleTest extends TestCase
     public function testBasicTest()
     {
         $this->assertTrue(true);
+
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+
+        $first = factory(Post::class)->create();
+
+        $secund = factory(Post::class)->create([
+            'created_at' => \Carbon\Carbon::now()->subMonth()
+        ]);
+
+        $posts = Post::archives();
+
+        $this->assertCount(2, $posts);
     }
 }
